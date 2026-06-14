e985fe00985c1fe36600e326178a57334d74c53b
emailbaruaku6-max <emailbaruaku6@gmail.com>
2026-06-14 07:17:31 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("PPLI Smelter App")),
-        body: const Center(child: Text("Sistem Siap Beroperasi")),
+        appBar: AppBar(title: const Text("PPLI Smelter - Data")),
+        body: const Center(child: Text("Menghubungkan ke Sheets...")),
       ),
     );
   }
