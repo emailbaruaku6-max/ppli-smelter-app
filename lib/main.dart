6019491da494730fab8adb8fd2f5060fa70efce5
emailbaruaku6-max <emailbaruaku6@gmail.com>
2026-06-14 07:23:50 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("PPLI Smelter - Data")),
-        body: const Center(child: Text("Menghubungkan ke Sheets...")),
+        body: const Center(child: Text("Menyiapkan Koneksi...")),
       ),
     );
+  }
+}
+
+// Fungsi ini nanti akan kita panggil untuk mengambil data
+Future<void> fetchData() async {
+  // Ganti ID di bawah ini dengan ID Google Sheets Baginda
+  final String sheetId = '1N5qEnYzJzEeKTv3nvrMyTrcTI-s-7gA3NE3Q4n2KjcA'; 
+  final String url = 'https://docs.google.com/spreadsheets/d/$sheetId/gviz/tq?tqx=out:json';
+  
+  final response = await http.get(Uri.parse(url));
+  
+  if (response.statusCode == 200) {
+    print("Data berhasil diambil!");
+  } else {
+    print("Gagal mengambil data.");
   }
 }
